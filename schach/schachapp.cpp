1cf96bc887b782c5f656ab4f5fa3e7f3c756326c
Carlos Barberá Domingo <carlos.barbera@tum.de>
2024-09-12 22:46:03 +0200

    Always play with bottom pieces

--- schach/schachapp.cpp
+++ schach/schachapp.cpp
@@ -23,8 +23,8 @@ SchachApp::SchachApp(QWidget *parent)
     , server(nullptr)
 {
     ui->setupUi(this);
-    initializeBoard();
-    setupChessBoard();
+    // initializeBoard();
+    // setupChessBoard();
     setDeviceController();
 
     on_cbHostClient_currentTextChanged("Client");
@@ -93,41 +93,44 @@ void SchachApp::initializeBoard()
         }
     }
 
+    if(isLocalPlayerWhite || isLocalGame) {
     // Map UI buttons to the grid
-    buttons[0][0] = ui->A1; buttons[0][1] = ui->B1; buttons[0][2] = ui->C1; buttons[0][3] = ui->D1;
-    buttons[0][4] = ui->E1; buttons[0][5] = ui->F1; buttons[0][6] = ui->G1; buttons[0][7] = ui->H1;
-    buttons[1][0] = ui->A2; buttons[1][1] = ui->B2; buttons[1][2] = ui->C2; buttons[1][3] = ui->D2;
-    buttons[1][4] = ui->E2; buttons[1][5] = ui->F2; buttons[1][6] = ui->G2; buttons[1][7] = ui->H2;
-    buttons[2][0] = ui->A3; buttons[2][1] = ui->B3; buttons[2][2] = ui->C3; buttons[2][3] = ui->D3;
-    buttons[2][4] = ui->E3; buttons[2][5] = ui->F3; buttons[2][6] = ui->G3; buttons[2][7] = ui->H3;
-    buttons[3][0] = ui->A4; buttons[3][1] = ui->B4; buttons[3][2] = ui->C4; buttons[3][3] = ui->D4;
-    buttons[3][4] = ui->E4; buttons[3][5] = ui->F4; buttons[3][6] = ui->G4; buttons[3][7] = ui->H4;
-    buttons[4][0] = ui->A5; buttons[4][1] = ui->B5; buttons[4][2] = ui->C5; buttons[4][3] = ui->D5;
-    buttons[4][4] = ui->E5; buttons[4][5] = ui->F5; buttons[4][6] = ui->G5; buttons[4][7] = ui->H5;
-    buttons[5][0] = ui->A6; buttons[5][1] = ui->B6; buttons[5][2] = ui->C6; buttons[5][3] = ui->D6;
-    buttons[5][4] = ui->E6; buttons[5][5] = ui->F6; buttons[5][6] = ui->G6; buttons[5][7] = ui->H6;
-    buttons[6][0] = ui->A7; buttons[6][1] = ui->B7; buttons[6][2] = ui->C7; buttons[6][3] = ui->D7;
-    buttons[6][4] = ui->E7; buttons[6][5] = ui->F7; buttons[6][6] = ui->G7; buttons[6][7] = ui->H7;
-    buttons[7][0] = ui->A8; buttons[7][1] = ui->B8; buttons[7][2] = ui->C8; buttons[7][3] = ui->D8;
-    buttons[7][4] = ui->E8; buttons[7][5] = ui->F8; buttons[7][6] = ui->G8; buttons[7][7] = ui->H8;
+        buttons[0][0] = ui->A1; buttons[0][1] = ui->B1; buttons[0][2] = ui->C1; buttons[0][3] = ui->D1;
+        buttons[0][4] = ui->E1; buttons[0][5] = ui->F1; buttons[0][6] = ui->G1; buttons[0][7] = ui->H1;
+        buttons[1][0] = ui->A2; buttons[1][1] = ui->B2; buttons[1][2] = ui->C2; buttons[1][3] = ui->D2;
+        buttons[1][4] = ui->E2; buttons[1][5] = ui->F2; buttons[1][6] = ui->G2; buttons[1][7] = ui->H2;
+        buttons[2][0] = ui->A3; buttons[2][1] = ui->B3; buttons[2][2] = ui->C3; buttons[2][3] = ui->D3;
+        buttons[2][4] = ui->E3; buttons[2][5] = ui->F3; buttons[2][6] = ui->G3; buttons[2][7] = ui->H3;
+        buttons[3][0] = ui->A4; buttons[3][1] = ui->B4; buttons[3][2] = ui->C4; buttons[3][3] = ui->D4;
+        buttons[3][4] = ui->E4; buttons[3][5] = ui->F4; buttons[3][6] = ui->G4; buttons[3][7] = ui->H4;
+        buttons[4][0] = ui->A5; buttons[4][1] = ui->B5; buttons[4][2] = ui->C5; buttons[4][3] = ui->D5;
+        buttons[4][4] = ui->E5; buttons[4][5] = ui->F5; buttons[4][6] = ui->G5; buttons[4][7] = ui->H5;
+        buttons[5][0] = ui->A6; buttons[5][1] = ui->B6; buttons[5][2] = ui->C6; buttons[5][3] = ui->D6;
+        buttons[5][4] = ui->E6; buttons[5][5] = ui->F6; buttons[5][6] = ui->G6; buttons[5][7] = ui->H6;
+        buttons[6][0] = ui->A7; buttons[6][1] = ui->B7; buttons[6][2] = ui->C7; buttons[6][3] = ui->D7;
+        buttons[6][4] = ui->E7; buttons[6][5] = ui->F7; buttons[6][6] = ui->G7; buttons[6][7] = ui->H7;
+        buttons[7][0] = ui->A8; buttons[7][1] = ui->B8; buttons[7][2] = ui->C8; buttons[7][3] = ui->D8;
+        buttons[7][4] = ui->E8; buttons[7][5] = ui->F8; buttons[7][6] = ui->G8; buttons[7][7] = ui->H8;
+    } else if(!isLocalPlayerWhite){
+        buttons[7][7] = ui->A1; buttons[7][6] = ui->B1; buttons[7][5] = ui->C1; buttons[7][4] = ui->D1;
+        buttons[7][3] = ui->E1; buttons[7][2] = ui->F1; buttons[7][1] = ui->G1; buttons[7][0] = ui->H1;
+        buttons[6][7] = ui->A2; buttons[6][6] = ui->B2; buttons[6][5] = ui->C2; buttons[6][4] = ui->D2;
+        buttons[6][3] = ui->E2; buttons[6][2] = ui->F2; buttons[6][1] = ui->G2; buttons[6][0] = ui->H2;
+        buttons[5][7] = ui->A3; buttons[5][6] = ui->B3; buttons[5][5] = ui->C3; buttons[5][4] = ui->D3;
+        buttons[5][3] = ui->E3; buttons[5][2] = ui->F3; buttons[5][1] = ui->G3; buttons[5][0] = ui->H3;
+        buttons[4][7] = ui->A4; buttons[4][6] = ui->B4; buttons[4][5] = ui->C4; buttons[4][4] = ui->D4;
+        buttons[4][3] = ui->E4; buttons[4][2] = ui->F4; buttons[4][1] = ui->G4; buttons[4][0] = ui->H4;
+        buttons[3][7] = ui->A5; buttons[3][6] = ui->B5; buttons[3][5] = ui->C5; buttons[3][4] = ui->D5;
+        buttons[3][3] = ui->E5; buttons[3][2] = ui->F5; buttons[3][1] = ui->G5; buttons[3][0] = ui->H5;
+        buttons[2][7] = ui->A6; buttons[2][6] = ui->B6; buttons[2][5] = ui->C6; buttons[2][4] = ui->D6;
+        buttons[2][3] = ui->E6; buttons[2][2] = ui->F6; buttons[2][1] = ui->G6; buttons[2][0] = ui->H6;
+        buttons[1][7] = ui->A7; buttons[1][6] = ui->B7; buttons[1][5] = ui->C7; buttons[1][4] = ui->D7;
+        buttons[1][3] = ui->E7; buttons[1][2] = ui->F7; buttons[1][1] = ui->G7; buttons[1][0] = ui->H7;
+        buttons[0][7] = ui->A8; buttons[0][6] = ui->B8; buttons[0][5] = ui->C8; buttons[0][4] = ui->D8;
+        buttons[0][3] = ui->E8; buttons[0][2] = ui->F8; buttons[0][1] = ui->G8; buttons[0][0] = ui->H8;
+    }
 
-//      buttons[7][7] = ui->A1; buttons[7][6] = ui->B1; buttons[7][5] = ui->C1; buttons[7][4] = ui->D1;
-//      buttons[7][3] = ui->E1; buttons[7][2] = ui->F1; buttons[7][1] = ui->G1; buttons[7][0] = ui->H1;
-//      buttons[6][7] = ui->A2; buttons[6][6] = ui->B2; buttons[6][5] = ui->C2; buttons[6][4] = ui->D2;
-//      buttons[6][3] = ui->E2; buttons[6][2] = ui->F2; buttons[6][1] = ui->G2; buttons[6][0] = ui->H2;
-//      buttons[5][7] = ui->A3; buttons[5][6] = ui->B3; buttons[5][5] = ui->C3; buttons[5][4] = ui->D3;
-//      buttons[5][3] = ui->E3; buttons[5][2] = ui->F3; buttons[5][1] = ui->G3; buttons[5][0] = ui->H3;
-//      buttons[4][7] = ui->A4; buttons[4][6] = ui->B4; buttons[4][5] = ui->C4; buttons[4][4] = ui->D4;
-//      buttons[4][3] = ui->E4; buttons[4][2] = ui->F4; buttons[4][1] = ui->G4; buttons[4][0] = ui->H4;
-//      buttons[3][7] = ui->A5; buttons[3][6] = ui->B5; buttons[3][5] = ui->C5; buttons[3][4] = ui->D5;
-//      buttons[3][3] = ui->E5; buttons[3][2] = ui->F5; buttons[3][1] = ui->G5; buttons[3][0] = ui->H5;
-//      buttons[2][7] = ui->A6; buttons[2][6] = ui->B6; buttons[2][5] = ui->C6; buttons[2][4] = ui->D6;
-//      buttons[2][3] = ui->E6; buttons[2][2] = ui->F6; buttons[2][1] = ui->G6; buttons[2][0] = ui->H6;
-//      buttons[1][7] = ui->A7; buttons[1][6] = ui->B7; buttons[1][5] = ui->C7; buttons[1][4] = ui->D7;
-//      buttons[1][3] = ui->E7; buttons[1][2] = ui->F7; buttons[1][1] = ui->G7; buttons[1][0] = ui->H7;
-//      buttons[0][7] = ui->A8; buttons[0][6] = ui->B8; buttons[0][5] = ui->C8; buttons[0][4] = ui->D8;
-//      buttons[0][3] = ui->E8; buttons[0][2] = ui->F8; buttons[0][1] = ui->G8; buttons[0][0] = ui->H8;
-
+    setupChessBoard();
 
 }
 
@@ -550,10 +553,12 @@ void SchachApp::on_cbHostClient_currentTextChanged(const QString &mode) {
         }
     } else if(mode == "Local") {
         isLocalGame = true;
-        ui->bConnect->setText("Starting Local Game");
+        ui->bConnect->setEnabled(false);
+        ui->bConnect->setText("Connect");
 
-        ui->bStart->setEnabled(false);
+        ui->bStart->setEnabled(true);
         ui->leIP->setEnabled(false);
+        ui->spnPort->setEnabled(false);
         ui->cbStartingPlayer->setEnabled(false);
 
         if(server) {
@@ -636,6 +641,7 @@ void SchachApp::gameStarted(bool ServerStarts, QString groupNumber) {
             //isLocalTurn = true;
             isLocalPlayerWhite = true;
         }
+        initializeBoard();
 }
 
 // Server version of gameStarted()
@@ -654,9 +660,13 @@ void SchachApp::on_cbStartingPlayer_currentTextChanged(const QString &startingPl
 void SchachApp::on_bStart_clicked()
 {
     ui->bStart->setEnabled(false);
-    bool ServerStarts = (ui->cbStartingPlayer->currentText() == "Server");
-    server->sendGameStart(ServerStarts);
-    updateNetzwerkConsole("Game start message sent. " + ui->cbStartingPlayer->currentText() + " starts.");
     startTurnTimer();
     updatecurrentPlayerLabel();
+    initializeBoard();
+
+    if(!isLocalGame) {
+        bool ServerStarts = (ui->cbStartingPlayer->currentText() == "Server");
+        server->sendGameStart(ServerStarts);
+        updateNetzwerkConsole("Game start message sent. " + ui->cbStartingPlayer->currentText() + " starts.");
+    }
 }
