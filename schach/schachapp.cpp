c19baab281fa484fc7bccf62ce1e87e52aaae964
Carlos Barbera Domingo <ge84bot@tum.de>
2024-09-16 20:01:21 +0200

    Fixed button send name only enables when connected

--- schach/schachapp.cpp
+++ schach/schachapp.cpp
@@ -775,8 +775,6 @@ void SchachApp::on_cbHostClient_currentTextChanged(const QString &mode) {
         ui->spnPort->setEnabled(true);
         ui->cbStartingPlayer->setEnabled(false);
         ui->bConnect->setEnabled(true);
-        ui->bSendName->setEnabled(true);
-        ui->lename->setEnabled(true);
 
         // Switch to Client mode
         if(server) {
@@ -812,8 +810,6 @@ void SchachApp::on_cbHostClient_currentTextChanged(const QString &mode) {
         ui->spnPort->setEnabled(true);
         ui->cbStartingPlayer->setEnabled(true);
         ui->bConnect->setEnabled(true);
-        ui->bSendName->setEnabled(true);
-        ui->lename->setEnabled(true);
 
         // Switch to Server mode
         if(client) {
@@ -903,6 +899,8 @@ void SchachApp::device_connected() {
     ui->bConnect->setText("Disconnect");
     ui->lstChat->setEnabled(true);
     ui->leSendChat->setEnabled(true);
+    ui->bSendName->setEnabled(true);
+    ui->lename->setEnabled(true);
 }
 
 void SchachApp::device_disconnected() {
@@ -912,6 +910,8 @@ void SchachApp::device_disconnected() {
     ui->bConnect->setText("Connect");
     ui->lstChat->setEnabled(false);
     ui->leSendChat->setEnabled(false);
+    ui->bSendName->setEnabled(false);
+    ui->lename->setEnabled(false);
 }
 
 void SchachApp::device_stateChanged(QAbstractSocket::SocketState state) {
@@ -1429,11 +1429,15 @@ void SchachApp::onChatMsgReceived(QString message) {
 }
 
 void SchachApp::on_client_connected() {
+    ui->bSendName->setEnabled(true);
+    ui->lename->setEnabled(true);
     ui->lstChat->setEnabled(true);
     ui->leSendChat->setEnabled(true);
 }
 
 void SchachApp::on_client_disconnected() {
+    ui->bSendName->setEnabled(false);
+    ui->lename->setEnabled(false);
     ui->lstChat->setEnabled(false);
     ui->leSendChat->setEnabled(false);
 }
